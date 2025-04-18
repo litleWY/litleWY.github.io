a9c0cc3204ae5eb211cc4b7d0288bfff64d8a8e3
wendi WanYan <147970000+litleWY@users.noreply.github.com>
2025-04-18 13:51:32 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,8 +21,8 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://litleWY.github.io',
-  base: '/my-blog'
+  site: "https://litleWY.github.io",
+  base: "/my-blog"
   trailingSlash: "always",
   integrations: [
     tailwind(
